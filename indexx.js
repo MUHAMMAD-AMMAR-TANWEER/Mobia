0ebeae04087f1b7adf24dcf0c64394921bbf32de
raheelbaig <68862554+raheelbaig@users.noreply.github.com>
2022-11-26 19:08:29 +0500

    update in functionality

--- indexx.js
+++ indexx.js
@@ -1,9 +1,9 @@
  //Map Initialization
  var startPoint = [51.5, -0.09];
  var map = L.map("map", {editable: true}).setView(startPoint, 13);
- var marker = [];
+ var markers = [];
  var polygon = [];
-
+var counter = 0
  var greenIcon = L.icon({
   iconUrl: 'SCI.png',
   shadowUrl: '',
@@ -36,13 +36,13 @@
 
 function addPolygon() {
   var line = L.polygon([
-    [51.508, -0.08],
+    [51.5, -0.09],
     [51.503, -0.06],
     [51.52 , -0.047],
   ], {
     color: "red",
     fillColor: "red"
-  }).addTo(map);
+  }).addTo(map).bindPopup("Here is Polygon!");
   line.enableEdit();
   
   map.on('editable:vertex:ctrlclick editable:vertex:metakeyclick', function (e) {
@@ -51,40 +51,71 @@ function addPolygon() {
   polygon.push(line)
 }
 
- marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
- circle.bindPopup("I am a circle.");
- polygon.bindPopup("I am a polygon.");
- 
- var popup = L.popup()
-     .setLatLng([51.513, -0.09])
-     .setContent("I am a standalone popup.")
-     .openOn(map);
+var popup = L.popup()
+.setLatLng([51.513, -0.09])
+.setContent("London.")
+.openOn(map);
 
-function addmarker() {
+// greenIcon.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+// circle.bindPopup("I am a circle.");
+// polygon.bindPopup("I am a polygon.");
 
-  var newMarker = new L.marker(["51.5", "-0.09"],{
-    draggable: true,
-    autoPan: true,
-    icon: greenIcon
-  });
-  console.log("sssssssssssssssssss",newMarker);
-  marker.push(newMarker);
-  console.log("latitude ==>",newMarker["_leaflet_id"]
-  );
-  console.log(marker);
-  map.addLayer(newMarker)
-}
+map.on('click', onMapClick);
 
 
-function removeMarker() {
-  console.log(marker[marker.length-1]["_leaflet_id"])
-  map.removeLayer(marker[marker.length-1]);
-  marker.pop()
-}
-map.removeLayer(marker)
+
+
+function onMapClick(e) {
+  counter+=1
+  console.log(e)
+  var marker = new L.Marker(e.latlng, {draggable:true}).bindPopup(`Marker is ${counter}`);
+  // counter+=1
+  map.addLayer(marker);
+  markers[marker._leaflet_id] = marker;
+  console.log(markers);
+  $('#overlay > ul').append('<li>Marker ' + String(counter) +' - <a href="#" class="remove" id="' + marker._leaflet_id + '">remove</a></li>');
+// Remove a marker
+$('.remove').on("click", function() {
+  // Remove the marker
+  map.removeLayer(markers[$(this).attr('id')]);
+  
+  // Remove the link
+  $(this).parent('li').remove();
+  
+  // Remove the marker from the array
+  delete markers[$(this).attr('id')]; 
+  
+});
+
+};
+
+
+// function addmarker() {
+
+//   var newMarker = new L.marker(["51.5", "-0.09"],{
+//     draggable: true,
+//     autoPan: true,
+//     icon: greenIcon
+//   }).bindPopup("Delete this marker </br> <button>delete</button>");
+//   console.log("sssssssssssssssssss",newMarker);
+//   marker.push(newMarker);
+//   console.log("latitude ==>",newMarker["_leaflet_id"]
+//   );
+//   console.log(marker);
+//   map.addLayer(newMarker)
+// }
+
+
+// function removeMarker() {
+//   console.log(marker[marker.length-1]["_leaflet_id"])
+//   map.removeLayer(marker[marker.length-1]);
+//   marker.pop()
+// }
+// map.removeLayer(marker)
 
 function deletePolygon() {
   console.log(polygon[polygon.length-1]["_leaflet_id"])
   map.removeLayer(polygon[polygon.length-1]);
   polygon.pop()
 }
+
